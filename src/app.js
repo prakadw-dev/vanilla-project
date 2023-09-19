f4d94e42d080c11896a724cb266db7bb4c7b3878
Sara <prakasitadewi@gmail.com>
2023-09-19 14:22:41 +1000

    Added icon
    Added icon based on the city searched

--- src/app.js
+++ src/app.js
@@ -40,6 +40,13 @@ function displayTemperature(response) {
 
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 apiKey = "6bfa54f242cbb59343d4e58db578dc61";
