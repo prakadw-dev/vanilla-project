df9f1ce2d7adc0019028fe7abaec2586c383df79
Sara <prakasitadewi@gmail.com>
2023-09-19 13:41:54 +1000

    Updated the time
    Current time of the "Last updated" and removed the console log in the function.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
-function displayTemperature(response) {
-  console.log(response.data);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
 
@@ -15,6 +37,9 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 apiKey = "6bfa54f242cbb59343d4e58db578dc61";
