499eb8c836bb46ac5d206c56286d63f77975550f
Sara <prakasitadewi@gmail.com>
2023-09-22 20:33:13 +1000

    Added API for showing forecast data
    including the day format, max and mix temp, and icon.

--- src/app.js
+++ src/app.js
@@ -22,29 +22,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-      <div class="forecast-date">${day}</div>
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+      <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-        src="http://openweathermap.org/img/wn/04d@2x.png"
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt="sunny"
         id="icon"
         width="38px"
         />
         <br />
         <div class="forecast-temp">
-        <span class="forecast-temp-max">20° </span>
-        <span class="forecast-temp-min">18°</span>
+        <span class="forecast-temp-max">${Math.round(
+          forecastDay.temp.max
+        )}° </span>
+        <span class="forecast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
       </div>
     </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
